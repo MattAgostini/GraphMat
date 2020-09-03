f4063275e717bde276212055b69fb63ddf578aa7
unknown <matt.agostini@mail.utoronto.ca>
2020-09-03 03:40:07 -0400

    redoing script

--- run_script.py
+++ run_script.py
@@ -20,11 +20,11 @@ for arg in sys.argv:
 
 graphs = [
     'as-skitter',
-    #'orkut',
-    #'higgs',
-    #'livejournal',
-    #'pokec',
-    #'stackoverflow',
+    'orkut',
+    'higgs',
+    'livejournal',
+    'pokec',
+    'stackoverflow',
 ]
 
 graph_prefix = {
@@ -36,7 +36,7 @@ graph_prefix = {
 start_vertex = {
     'bfs' : {
         'as-skitter': {878248, 1093773, 1040066, 1529161, 1105468, 1543502},
-        'orkut': {2062367, 767779, 1805450, 1060076, 424425, 641114, },
+        'orkut': {2062367, 767779, 1805450, 1060076, 424425, 641114},
         'higgs': {165486, 15147, 288568, 17220, 127341, 328483},
         'livejournal': {3903641, 4158378, 1486101, 467386, 1875102, 1966836},
         'pokec': {858951, 438160, 1385063, 793905, 310461, 300989},
@@ -50,7 +50,14 @@ start_vertex = {
         'pokec': {247882, 246033, 1092414, 720811, 255164, 139421},
         'stackoverflow': {3214351, 2446213, 3198986, 1313602, 32228, 824045},
     },
-    'pr' : {}
+    'pr' : {
+        'as-skitter': {878248, 1093773},
+        'orkut': {2062367, 767779},
+        'higgs': {165486, 15147},
+        'livejournal': {3903641, 4158378},
+        'pokec': {858951, 438160},
+        'stackoverflow': {5515818, 3554183},
+    }
 }
 
 app_command = {
@@ -75,33 +82,30 @@ def collectGraphs(app, threads, optionalArgs = []):
             optional = ' '.join(optionalArgs)
             graphfile = 'graphs/' + graph_prefix[app] + graph + '.g'
 
-
             start_vtx = []
             if graph in start_vertex[app]:
                 start_vtx = start_vertex[app][graph]
-            else:
-                start_vtx = [0] # PageRank
 
             for start_v in start_vtx:
                 start_v = start_v + 1 # Need this because GraphMat edges start at 1 instead of 0
 
-            #for i in range(5):
+                for i in range(5): # Doing each start vertex 5 times
 
-                print(f'Running {app} with {threacount} thread(s) on {graphfile} with start {start_v}...')
+                    print(f'Running {app} with {threacount} thread(s) on {graphfile} with start {start_v}...')
 
-                runCommand(f'{app_command[app]} {graphfile} {start_v} > tmp_out')
+                    runCommand(f'{app_command[app]} {graphfile} {start_v} > tmp_out')
 
-                if app == 'pr':
-                    runCommand(f'awk \'$1 == "Completed" {{print $2}}\' tmp_out | tail -n 1 > tmp_out2')
-                    runCommand(f'echo "," >> tmp_out2')
-                    runCommand(f'awk \'$1 == "PR" {{print $4}}\' tmp_out >> tmp_out2')
-                    runCommand(f'tr -d \'\\n\' < tmp_out2 > tmp_out3')
-                    runCommand(f'echo "" >> tmp_out3')
-                    runCommand(f'echo -n "{app},{threacount},{graph}," | cat - tmp_out3 >> {out_file}')
+                    if app == 'pr':
+                        runCommand(f'awk \'$1 == "Completed" {{print $2}}\' tmp_out | tail -n 1 > tmp_out2')
+                        runCommand(f'echo "," >> tmp_out2')
+                        runCommand(f'awk \'$1 == "PR" {{print $4}}\' tmp_out >> tmp_out2')
+                        runCommand(f'tr -d \'\\n\' < tmp_out2 > tmp_out3')
+                        runCommand(f'echo "" >> tmp_out3')
+                        runCommand(f'echo -n "{app},{threacount},{graph}," | cat - tmp_out3 >> {out_file}')
 
-                else:
-                    runCommand(f'awk \'$1 == "Time" {{print $3}}\' tmp_out > tmp_out2')
-                    runCommand(f'echo -n "{app},{threacount},{graph},1," | cat - tmp_out2 >> {out_file}')
+                    else:
+                        runCommand(f'awk \'$1 == "Time" {{print $3}}\' tmp_out > tmp_out2')
+                        runCommand(f'echo -n "{app},{threacount},{graph},1," | cat - tmp_out2 >> {out_file}')
 
 #threads = [4, 8, 14]
 threads = [14]
